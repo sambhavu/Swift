37ea5250c333653ea13400f7e6bd5133802f346b
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-04-11 22:55:41 +0530

    Create function_.swift

---
+++ function_.swift
@@ -0,0 +1,21 @@
+func sayHello(personName: String) -> String
+{
+   let greeting="Hello, " + personName + "!"
+   return greeting
+} 
+
+print(sayHello(personName: "David"))
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+   var currMin = array[0]
+   var currMax = array[0]
+   for value in array[1..<array.count] {
+	  if value < currMin {
+		currMin = value
+	  } else if value > currMax {
+		 currMax = value
+	  }
+   }
+  return (currMin, currMax)
+}
+print(minMax(array: [1, 2, 3, 4, -6, 11]))
