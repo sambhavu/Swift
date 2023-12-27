27ba7799f9b08316325592ab72ecac217f5d3dbf
sambhavu <30390294+sambhavu@users.noreply.github.com>
2023-12-26 23:35:42 -0500

    Create helloworld.swift

---
+++ helloworld.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Iceberg
+//
+//  Created by Sambhav Upadhyay on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
